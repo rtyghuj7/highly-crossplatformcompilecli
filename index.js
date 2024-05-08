fe989c484671ccbb88d4701e6c8872c47a65b938
rtyghuj7 <js1kekefoul@hotmail.com>
2024-05-09 00:43:34 +0700

    start project

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
-function coinChange(coins, amount) {
-  const dp = new Array(amount + 1).fill(amount + 1);
-  dp[0] = 0;
-  for (const coin of coins) {
-    for (let i = coin; i <= amount; i++) {
-      dp[i] = Math.min(dp[i], dp[i - coin] + 1);
-    }
-  }
-  return dp[amount] > amount ? -1 : dp[amount];
-}
+const flatten = (arr, depth = 1) =>
+  arr.reduce(
+    (a, v) =>
+      a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v),
+    [],
+  );
