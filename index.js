4c60596b5f76f02713f087676319a0053dc510dd
rtyghuj7 <js1kekefoul@hotmail.com>
2024-05-09 08:08:58 +0700

    feat commit

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
-const flatten = (arr, depth = 1) =>
-  arr.reduce(
-    (a, v) =>
-      a.concat(depth > 1 && Array.isArray(v) ? flatten(v, depth - 1) : v),
-    [],
-  );
+function minMeetingRooms(intervals) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const minHeap = new MinHeap();
+  for (const interval of intervals) {
+    if (minHeap.size() > 0 && minHeap.peek() <= interval[0]) {
+      minHeap.pop();
+    }
+    minHeap.push(interval[1]);
+  }
+  return minHeap.size();
+}
