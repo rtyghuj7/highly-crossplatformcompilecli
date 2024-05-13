22763ee892e75055af02ad3875f9a3c3175bcdfb
rtyghuj7 <js1kekefoul@hotmail.com>
2024-05-14 05:24:52 +0700

    start project commit

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function sayHi() {
-  console.log("Hi!");
+function mergeTwoLists(l1, l2) {
+  if (!l1) return l2;
+  if (!l2) return l1;
+  if (l1.val < l2.val) {
+    l1.next = mergeTwoLists(l1.next, l2);
+    return l1;
+  } else {
+    l2.next = mergeTwoLists(l1, l2.next);
+    return l2;
+  }
 }
-
-setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
