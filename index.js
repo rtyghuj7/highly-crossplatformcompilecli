0b08befec7a415286feebb535b67f1008376462b
rtyghuj7 <js1kekefoul@hotmail.com>
2024-05-14 18:58:39 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-function mergeTwoLists(l1, l2) {
-  if (!l1) return l2;
-  if (!l2) return l1;
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
-  } else {
-    l2.next = mergeTwoLists(l1, l2.next);
-    return l2;
-  }
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
 }
